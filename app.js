1dc1b3c05ac826c02bd29d3ab582a6c273f9d20b
Manisha Singh <80120667+manisha-singh22@users.noreply.github.com>
2021-08-01 09:58:44 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express=require("express");
 var bodyParser=require("body-parser"); 
 
 const mongoose = require('mongoose'); 
-mongoose.connect('mongodb://localhost:27017/gfg'); 
+mongoose.connect('mongodb://localhost:3000'); 
 var db=mongoose.connection; 
 db.on('error', console.log.bind(console, "connection error")); 
 db.once('open', function(callback){ 
